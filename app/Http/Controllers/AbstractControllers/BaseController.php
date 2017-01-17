391bf693b1041c33ca0d0998fc00f983f0e4d02b
Igor <kovalevgr@gmail/com>
2017-01-17 05:27:58 +0200

    Базавый контролер для Admin, Internet etc.

---
+++ app/Http/Controllers/AbstractControllers/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\AbstractControllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+abstract class BaseController extends Controller
+{
+    //
+}
